05779b65f890b89bf3db7d2ac8fb5f40dcc94de4
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-26 11:48:47 -0300

    soma divisao subtração multiplicação

---
+++ Soma, Divisao, subtração, multiplicação/Soma, Divisao, subtração, multiplicação/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soma__Divisao__subtração__multiplicação
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int divi, num, num2, multi, sub, soma;
+            Console.Write("Digite o primeiro numero: ");
+            num = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo numero: ");
+            num2 = int.Parse(Console.ReadLine());
+            sub = num - num2;
+            multi = num * num2;
+            soma = num + num2;
+            divi = num / num2;
+            Console.WriteLine("O resultado é: " + sub);
+            Console.WriteLine("\n O resultado é: " + multi);
+            Console.WriteLine("\n O resultado é: " + divi);
+            Console.WriteLine("\n O resultado é: " + soma);
+
+ 
+
+
+
+
+
+
+
+        }
+    }
+}

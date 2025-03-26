a06d177c08bb01813b5497909e4ce357ac52bae8
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-26 11:30:10 -0300

    Pescador

---
+++ Pescador/Pescador/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pescador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso;
+            double sobra;
+            Console.Write("digite o peso pescado: ");
+            peso = int.Parse(Console.ReadLine());
+            sobra = (peso - 50) * 4;
+            Console.WriteLine(sobra);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

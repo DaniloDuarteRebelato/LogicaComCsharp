59ab038e310ebbd3e7652eb90ef304ef7e77b9bf
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-27 10:56:49 -0300

    Keno

---
+++ Keno/Keno/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double pd, pe;
+
+            Console.Write("keno, digite o tamanho da sua perna direita: ");
+            pd = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("keno, digite o tamanho da sua perna esquerda: ");
+            pe = int.Parse(Console.ReadLine());
+            if(pe > pd)
+            {
+                Console.WriteLine("ala o cara é cotoco da perna direita kakakakak lol ");
+            }
+            else
+            {
+                Console.WriteLine("normal");
+            }
+                
+            
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

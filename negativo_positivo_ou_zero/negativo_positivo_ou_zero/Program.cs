495a9062dcfc1bc0397941548a0473d22d2720b0
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-27 11:06:45 -0300

    Negativo 0 ou positivo

---
+++ negativo_positivo_ou_zero/negativo_positivo_ou_zero/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negativo_positivo_ou_zero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1;
+            Console.Write("digite numero desejado: ");
+            num1 = double.Parse(Console.ReadLine());
+
+            if(num1 > 0)
+            {
+                Console.WriteLine(" seu numero é positivo hehe ");
+            }else if (num1 < 0)
+            {
+                Console.WriteLine("seu numero é negativo bb ");
+            }
+            else
+            {
+                Console.WriteLine("seu numero é 0 b");
+            }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

ca9e56a47c691e77bbf6cbca1981850847fb30a3
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-27 11:39:37 -0300

    Idade para dirigir

---
+++ Idade_para_dirigir/Idade_para_dirigir/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idade_para_dirigir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double idade;
+
+
+            Console.Write("insira sua idade ");
+            idade = double.Parse(Console.ReadLine());
+            if(idade > 17)
+            {
+                Console.WriteLine("pode dirigir");
+            }else
+            {
+                Console.WriteLine("não pode dirigir");
+            }
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

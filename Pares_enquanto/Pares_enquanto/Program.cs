b762a56edc416ae246be6d4c0e3a746a948f629d
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-27 09:36:31 -0300

    Pares enquanto

---
+++ Pares_enquanto/Pares_enquanto/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pares_enquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+
+
+            while(contador < 100){
+                contador = contador + 2;
+            }
+
+            Console.WriteLine(" " + contador);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

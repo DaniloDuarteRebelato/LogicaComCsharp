d277d4668ced681de445c07310660557e22e7822
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-27 09:59:42 -0300

    Impar_para

---
+++ Impar_Para/Impar_Para/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impar_Para
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int impar;
+
+          
+            for(impar = 1; impar < 100; impar++ ){
+                impar = impar + 4 / 2;
+                Console.WriteLine("Números impars: " + impar, "\n");
+            }
+               
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

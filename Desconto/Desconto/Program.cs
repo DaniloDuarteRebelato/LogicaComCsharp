5722fb00d699c0c8f90c98843f1cbc3339e648cd
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-27 11:20:52 -0300

    Desconto

---
+++ Desconto/Desconto/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desconto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num1, des, total;
+            Console.Write("digite o numero ");
+            num1 = double.Parse(Console.ReadLine());
+            if(num1 >= 100)
+            {
+                des = num1 * 0.10;
+                total = num1 - des;
+                Console.WriteLine(des);
+            }else
+            {
+                des = num1 * 0.05;
+                total = num1 - des;
+                Console.WriteLine(des);
+            }
+                
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

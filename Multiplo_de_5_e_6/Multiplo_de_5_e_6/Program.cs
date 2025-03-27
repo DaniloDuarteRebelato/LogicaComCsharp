d3ddd13427102ab9e1fb56fb2a6542ac187cf02a
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-27 11:17:02 -0300

    Multiplo de 3 e 5

---
+++ Multiplo_de_5_e_6/Multiplo_de_5_e_6/Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplo_de_5_e_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num3, num5;
+
+            Console.Write("digite o numero ");
+            num1 = double.Parse(Console.ReadLine());
+
+
+
+            if(num1 % 5 == 0)
+            {
+                Console.WriteLine("numero é multiplo de 5 ");
+            }else if (num1 % 3 == 0)
+            {
+                Console.WriteLine("numero é multiplo de 3 ");
+
+            }
+            else
+            {
+                Console.WriteLine("ne nao ");
+            }
+                
+    
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

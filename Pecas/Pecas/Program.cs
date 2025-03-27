3014bec004113e13b4203761a91a5a7225347e47
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-27 10:35:55 -0300

    Peças

---
+++ Pecas/Pecas/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pecas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pecas;
+            int total;
+            int resultado;
+
+            Console.Write("Quantidades de peças armazenadas: ");
+            pecas = int.Parse(Console.ReadLine());  
+            for(total = 400; pecas < 400; pecas++)
+            {
+                resultado = total - pecas;
+                Console.WriteLine("O total de peças descartadas foram: ", resultado);
+            }
+               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

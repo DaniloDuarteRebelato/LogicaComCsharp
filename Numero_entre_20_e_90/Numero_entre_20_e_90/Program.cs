1bc289efd4d3aef15cabda60f10570bf91da2caf
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-27 09:10:35 -0300

    20 e 90

---
+++ Numero_entre_20_e_90/Numero_entre_20_e_90/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero_entre_20_e_90
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            Console.Write("Digite um número inteiro: ");
+            num1 = int.Parse(Console.ReadLine());
+            if(num1 >= 21 && num1 <= 89)
+            {
+                Console.WriteLine("Este numero esta entre 20 e 90");
+            }
+            else
+            {
+                Console.WriteLine("Este numero nao esta entre 20 e 90");
+            }
+            
+    
+      
+           
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

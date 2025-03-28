903e3c24daf26c295c19c95db38b541b475a629a
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-28 09:24:00 -0300

    Numeros_iguais

---
+++ Numeros_iguais/Numeros_iguais/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_iguais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num1, num2;
+
+            Console.Write("Digite um número: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            if(num1 == num2)
+            {
+                Console.WriteLine("Números iguais");
+            }
+            else
+            {
+                Console.WriteLine("Esse números não são iguais");
+            }
+               
+    
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

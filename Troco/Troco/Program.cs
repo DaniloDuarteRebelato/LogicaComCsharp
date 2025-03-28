b5cc9dde97dec6ae7c6c8f4d5d2ebe7399ff4cb7
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-28 08:46:50 -0300

    Troco

---
+++ Troco/Troco/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            Console.Write("Digite o valor: ");
+            valor = int.Parse(Console.ReadLine());
+
+
+            if(valor >= 100)
+            {
+                Console.WriteLine("1 nota de 100\n");
+                valor = valor - 100;
+            }
+            if(valor >= 50)
+            {
+                Console.WriteLine("1 nota de 50\n");
+                valor = valor - 50;
+            }if(valor >= 20)
+            {
+                Console.WriteLine("1 nota de 20\n");
+                valor = valor - 20;
+            }if(valor >= 10)
+            {
+                Console.WriteLine("1 nota de 10\n");
+                valor = valor - 10;
+            }if(valor >= 5)
+            {
+                Console.WriteLine("1 nota de 5\n");
+                valor = valor - 5;
+            }if (valor >= 2)
+            {
+                Console.WriteLine("1 nota de 2\n");
+                valor = valor - 2;
+            }if (valor >= 1)
+            {
+                Console.WriteLine("1 nota de 1\n");
+                valor = valor - 1;
+            }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

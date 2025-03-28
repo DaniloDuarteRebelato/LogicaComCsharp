41eb620769b1616fc75684b5431a18a05aaf9704
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-28 11:21:14 -0300

    Salario minimo

---
+++ Salario_minimo/Salario_minimo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salario_minimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double user, min = 1518.00, resultado;
+
+            Console.Write("Digite o seu salario: ");
+            user = double.Parse(Console.ReadLine());
+            resultado = user / min;
+            Console.WriteLine(resultado);
+            
+       
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

25b957b1117ae4b80e5a32ee12be2a6a5f863d6f
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-28 08:55:48 -0300

    Dia_da_semana

---
+++ Dia_da_semana/Dia_da_semana/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dia_da_semana
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int diaDaSemana;
+
+            Console.Write("Digite o numero da semana: ;");
+            diaDaSemana = int.Parse(Console.ReadLine());
+
+            if(diaDaSemana > 7)
+            {
+                Console.WriteLine("Numero da semana invalida");
+
+
+            }else if (diaDaSemana == 1 || diaDaSemana == 6)
+            {
+                Console.WriteLine("Final de semana");
+
+            }
+            else
+            {
+                Console.WriteLine("Dia da semana normal");
+            }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

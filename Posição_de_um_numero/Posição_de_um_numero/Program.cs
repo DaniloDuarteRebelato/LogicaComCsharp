0d38da93b2769cf50f7a9c1632ad762312ff313b
DaniloDuarteRebelato <141834302+DaniloDuarteRebelato@users.noreply.github.com>
2025-03-28 09:42:19 -0300

    Posição de um numero

---
+++ Posição_de_um_numero/Posição_de_um_numero/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posição_de_um_numero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+
+            Console.Write("Digite um número entre 1 á 20: ");
+            num = double.Parse(Console.ReadLine());
+
+            if(num >= 1 && num <= 10)
+            {
+                Console.WriteLine("O número ", num, " está entre 1 á 10.");
+            }else if (num >= 11 && num <= 20)
+            {
+                Console.WriteLine("O número ", num, " está entre 11 á 20.");
+
+
+            }else if(num > 20)
+            {
+                Console.WriteLine("O número ", num, " está acima de 20.");
+            }
+            else
+            {
+                Console.WriteLine("ERRO! Não foi digitado um número válido.");
+            }
+
+
+
+
+       
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
